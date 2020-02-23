22858ef62ec39e6734de125e1fe2879c10eb0c9a
Fatih Karahan <hseyin97@gmail.com>
2020-02-23 18:28:09 +0300

    Connected to the database

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 /* eslint-disable no-console */
+const mongoose = require('mongoose');
 require('dotenv').config({ path: './config.env' });
 
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('Database connection successful.'))
+  .catch(err => console.log(err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
