aeadca871cfdd8358c6ea2d3744df99ba127a652
Fatih Karahan <hseyin97@gmail.com>
2020-02-23 17:57:56 +0300

    Initialized server

---
+++ index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-console */
+require('dotenv').config({ path: './config.env' });
+
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App is on port: ${port}`);
+});
